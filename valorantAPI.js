694c1838ee3878dac3974b6786842c265410bb12
Alexander Darby <dandarby228@gmail.com>
2023-01-23 15:04:11 -0500

    Added some error handling.

--- valorantAPI.js
+++ valorantAPI.js
@@ -9,10 +9,9 @@ const app = express();
 
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
-app.use(express.json({limit: '1mb'}));
-
-
-app.post('/api',(request,response) => {    
+app.use(express.json({ limit: '1mb' }));
+//Post request for valorant
+app.post('/api_val', (request, response) => {
     console.log("Request reccieved");
     console.log(request.body)
 
@@ -23,60 +22,54 @@ app.post('/api',(request,response) => {
         tag: request.body.tag
     })
     */
+    getAccount(request.body.username, request.body.tag, false)
 
-    async function getAccount(account, tag, force ){
-   
-        let data
-        data = await testAPI.getAccount({
-    
-            name: account,
-            tag: tag,
-            force: false,
-        
-        })
+    async function getAccount(account, tag, force) {
+        let data;
+        //What is here is an attempt at error handling, will try to find a better method.
+        try {
+            data = await testAPI.getAccount({
 
-        
+                name: account,
+                tag: tag,
+                force: false,
+            })
+            response.json({
+                status: "successful",
+                username: data.data.name,
+                tag: data.data.tag,
+                region: data.data.region,
+                ratelimits: data.ratelimits
+            })
+            console.log("Sucessful!")
+            console.log(data.ratelimits)
+        } catch (error) {
 
-        response.json({
-            status: "successful",
-            username: data.data.name,
-            tag: data.data.tag,
-            region: data.data.region
-        });
+            let code
+            let msg;
+            try {
+                code = data.error[0].code;
+                msg = data.error[0].details;
+            } catch (next_error) {
+                console.log("No error code from server: ", next_error + "\n" + "Original error message:", error)
+            }
 
-        console.log(data);
+            console.log("Error from try/catch: ", error + "\n" + "Error code from API: ", code + "\n" + "Error details: ", msg);
+
+            /*
+            if(data.data != "null"){
+            console.error("error: ", error.message)
+            }       
+            else{
+            console.error("Console Error", error + "\n" + "Code from api", data.error[0].code)
+            }
+            */
+        }
     }
-
-    async function getmatches(){
-    let data
-    let conversion
-
-    data = await testAPI.getMatches({
-        name: "Bear",
-        tag: "1507",
-        region: "na"
-    });
-
-    for(let x in data.data){
-
-        console.log(data.data[x].metadata);
-
-    }
-
-    console.log(data.ratelimits);
-
-}
-
-    getAccount(request.body.username, request.body.tag, false);
 
 });
 
-
-
-
-
-
-
+/*
 async function getmatches(){
     let data
     let conversion
@@ -112,3 +105,4 @@ async function getMMR(){
     }
 }
 
+*/
